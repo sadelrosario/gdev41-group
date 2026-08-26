742e0addaa267d8831ec6794e7a05cf7acdfb423
Kuma <55661040+NxKuma@users.noreply.github.com>
2026-08-27 05:12:05 +0800

    Added Extra Stuff
    - Enemy Movement
    - Fog-of-War
    - RPG Battle

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <windows.h>
+#include <vector>
+#include <cstdlib>
 using namespace std;
 
-void drawGrid(int width, int height, int playerPosX, int playerPosY, int enemyPosX, int enemyPosY) {
+struct Position {
+    int x;
+    int y;
+};
+
+void drawGrid(int width, int height, int playerPosX, int playerPosY, int enemyPosX, int enemyPosY, vector<Position> clearedPositions) {
+    //clear the screen
     // error checker
     if (playerPosX > width || playerPosY > height || enemyPosX > width || enemyPosY > height) {
         cout << "INVALID SETTINGS\n";
@@ -21,17 +30,30 @@ void drawGrid(int width, int height, int playerPosX, int playerPosY, int enemyPo
     for (int y = 0; y < height; y++) { // column height
         for (int x = 0; x < width; x++) { // 
             // check units' position
-            if ((playerPosX == x) && (playerPosY == y) && (enemyPosX == x) && (enemyPosY == y)) {
-                grid += "|PE";
-            } 
-            else if ((playerPosX == x) && (playerPosY == y)) {
-                grid += "|P ";
-            } 
-            else if ((enemyPosX == x) && (enemyPosY == y)) {
-                grid += "| E";
+            bool posCleared = false;
+            for (const Position& pos : clearedPositions) {
+                if (pos.x == x && pos.y == y) {
+                    posCleared = true;
+                    break;
+                }
             }
-            else {
-                grid += "|  ";
+
+            if (!posCleared) {
+                grid += "|\u2588\u2588";
+            } 
+            else{
+                if ((playerPosX == x) && (playerPosY == y) && (enemyPosX == x) && (enemyPosY == y)) {
+                    grid += "|PE";
+                } 
+                else if ((playerPosX == x) && (playerPosY == y)) {
+                    grid += "|P ";
+                } 
+                else if ((enemyPosX == x) && (enemyPosY == y)) {
+                    grid += "| E";
+                }
+                else {
+                    grid += "|  ";
+                }
             }
         }
         grid += "|\n";
@@ -46,6 +68,52 @@ void drawGrid(int width, int height, int playerPosX, int playerPosY, int enemyPo
     // print everything
     cout << grid << endl;
 }
+
+void drawBattleUI(int playerHP, int playerDmg,string playerAction, int enemyHP, int enemyDmg, string enemyAction){
+    string battleUI = "";
+    for (int y = 0; y < 7; y++) {
+
+        if (y == 0 || y == 2 || y == 4 || y == 6) {
+            battleUI += "--------------------\n";
+        }
+        else if (y == 1) {
+            if (playerHP <= 0) {
+                battleUI += "|   X            E   |\n";
+            } else if (enemyHP <= 0) {
+                battleUI += "|   P            X   |\n";
+            } else {
+                if ((playerAction == "a" || playerAction == "attack") && (enemyAction == "a")) {
+                    battleUI += "|   P  >      <  E   |\n";
+                } else if ((playerAction == "d" || playerAction == "defend") && (enemyAction == "a")) {
+                    battleUI += "|   P  |      <  E   |\n";
+                } else if ((playerAction == "a" || playerAction == "attack") && (enemyAction == "d")) {
+                    battleUI += "|   P  >      |  E   |\n";
+                } else if ((playerAction == "d" || playerAction == "defend") && (enemyAction == "d")) {
+                    battleUI += "|   P  |      |  E   |\n";
+                }else{
+                    battleUI += "|   P            E   |\n";
+                }
+            }
+        }
+        else if (y == 3) {
+            battleUI += "| HP: " + to_string(playerHP) + " ---- HP: " + to_string(enemyHP) + " |\n";
+        }
+        else if (y == 5) {
+            battleUI += "|- ATK(a) - DEF(d) -|\n";
+        }
+    }
+    cout << battleUI;
+    if ((playerAction == "a" || playerAction == "attack") && (enemyAction == "a")) {
+        cout << "Both attack each other dealing " << playerDmg << " DMG and " << enemyDmg << " DMG respectively.\n";
+    } else if ((playerAction == "d" || playerAction == "defend") && (enemyAction == "a")) {
+        cout << "Player got hurt defending Enemy's attack!\n";
+    } else if ((playerAction == "a" || playerAction == "attack") && (enemyAction == "d")) {
+        cout << "Enemy got hurt defending Player's attack!\n";
+    } else if ((playerAction == "d" || playerAction == "defend") && (enemyAction == "d")) {
+        cout << "Both defends as nothing happens!\n";
+    }
+}
+
 
 int warpMovement(string direction, int position, int gridSize) { 
     // handles positive direction
@@ -62,14 +130,47 @@ int warpMovement(string direction, int position, int gridSize) {
     return position;
 }
 
-int main() {
+void damageProcess(string playerAction, int playerDmg, int& playerHP, string enemyAction, int enemyDmg, int& enemyHP) {
+    if ((playerAction == "a" || playerAction == "attack") && (enemyAction == "a")) {
+        enemyHP -= playerDmg;
+        playerHP -= enemyDmg;
+    }
+    else if ((playerAction == "a" || playerAction == "attack") && (enemyAction == "d")) {
+        enemyHP -= rand() % 5 + 2;
+    }
+    else if ((playerAction == "d" || playerAction == "defend") && (enemyAction == "a")) {
+        playerHP -= rand() % 5 + 2;
+    }
+}
 
+void toLower(string& str){
+    for (char& c : str) {
+        if (c >= 'A' && c <= 'Z') {
+            c += 32;
+        }
+    }
+}
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
     // get input from fi
     ifstream file("settings.txt");
     
     int gridWidth, gridHeight;
+    
     int playerPosX, playerPosY;
+    int playerHealth = 100;
+    int playerDmg = 10;
+    
     int enemyPosX, enemyPosY;
+    int enemyHealth = 100;
+    int enemyDmg = 20;
+
+    int movementBuffer = 0;
+    bool engageBattle = false;
+
+    vector<Position> clearedPositions;
+    string inputLines[6] = {"w", "e", "s", "n", "a","d"};
 
     file >> gridWidth >> gridHeight;
     file.ignore(256, '\n');
@@ -80,7 +181,8 @@ int main() {
     // draw grid with initial settings first
     cout << "Text-Based Game\n";
     cout << "LOADING GRID...\n";
-    drawGrid(gridWidth, gridHeight, playerPosX, playerPosY, enemyPosX, enemyPosY);
+    clearedPositions.push_back({playerPosX, playerPosY});
+    drawGrid(gridWidth, gridHeight, playerPosX, playerPosY, enemyPosX, enemyPosY, clearedPositions);
 
     // start game loop
     while(true) {
@@ -88,40 +190,88 @@ int main() {
         string lineInput;
         getline(cin, lineInput);
         string input;
+        string enemyAction;
         stringstream ss(lineInput);
     
         // parse input line per string
-        while (getline(ss, input, ' ')) {            
-            if (input == "w" || input == "west" || input == "e" || input == "east") {
+        while (getline(ss, input, ' ')) {
+            toLower(input);
+            if (!engageBattle) {
+                if (input == "w" || input == "west" || input == "e" || input == "east") {
                 // horizontal movement
                 playerPosX = warpMovement(input, playerPosX, gridWidth);
-            }
-            else if (input == "s" || input == "south" || input == "n" || input == "north") {
-                // vertical movement
-                playerPosY = warpMovement(input, playerPosY, gridHeight);
-            }       
-            else if (input == "a" || input == "attack") {
-                // attack input
-                if ((playerPosX == enemyPosX) && (playerPosY == enemyPosY)) {
-                    cout << "Enemy has been slayed!\n";
+                clearedPositions.push_back({playerPosX, playerPosY});
+                if (movementBuffer > 1) {
+                    int rnd = rand() % 2;
+                    enemyPosX = warpMovement(inputLines[rnd], enemyPosX, gridWidth); 
+                    movementBuffer = 0;
+                }
+                
+                }
+                else if (input == "s" || input == "south" || input == "n" || input == "north") {
+                    // vertical movement
+                    playerPosY = warpMovement(input, playerPosY, gridHeight);
+                    clearedPositions.push_back({playerPosX, playerPosY});
+                    if (movementBuffer > 1) {
+                        int rnd = rand() % 2 + 2;
+                        enemyPosY = warpMovement(inputLines[rnd], enemyPosY, gridHeight); 
+                        movementBuffer = 0;
+                    }
+                }       
+                else if (input == "a" || input == "attack") {
+                    // attack input
+                    if ((playerPosX == enemyPosX) && (playerPosY == enemyPosY)) {
+                        cout << "Engaged in Battle!\n";
+                        engageBattle = true;
+                        // exit(1);
+                    } 
+                    else {
+                        cout << "There are no enemies to attack.\n";
+                    }
+                }
+                else if (input == "exit") {
+                    cout << "Player has left the game. Sayonara!\n";
                     exit(1);
                 } 
                 else {
-                    cout << "There are no enemies to attack.\n";
+                    cout << "INVALID INPUT\n";
+                    break;
+                }
+                movementBuffer++;
+            }else{
+                if(input == "a" || input == "attack") {
+                    enemyAction = (rand() % 2 == 0) ? "a" : "d";
+                    damageProcess(input, playerDmg, playerHealth, enemyAction, enemyDmg, enemyHealth);
+                } 
+                else if (input == "d" || input == "defend") {
+                    enemyAction = (rand() % 2 == 0) ? "a" : "d";
+                    damageProcess(input, playerDmg, playerHealth, enemyAction, enemyDmg, enemyHealth);
+                }else if (input == "exit") {
+                    cout << "Player has left the battle. Sayonara!\n";
+                    exit(1);
+                } else {
+                    cout << "INVALID INPUT\n";
+                    break;
                 }
             }
-            else if (input == "exit") {
-                cout << "Player has left the game. Sayonara!\n";
-                exit(1);
-            } 
-            else {
-                cout << "INVALID INPUT\n";
-                break;
-            }
-
+            
         }
 
-        drawGrid(gridWidth, gridHeight, playerPosX, playerPosY, enemyPosX, enemyPosY);
+
+        if (!engageBattle){
+            drawGrid(gridWidth, gridHeight, playerPosX, playerPosY, enemyPosX, enemyPosY, clearedPositions);
+        } else{
+            if (playerHealth <= 0) {
+                drawBattleUI(playerHealth, playerDmg,input, 0, enemyDmg, enemyAction);
+                cout << "Player has been defeated! Game Over.\n";
+                exit(1);
+            } else if (enemyHealth <= 0) {
+                drawBattleUI(playerHealth, playerDmg,input, 0, enemyDmg, enemyAction);
+                cout << "Enemy has been defeated! You win!\n";
+                exit(1);
+            }
+            drawBattleUI(playerHealth, playerDmg,input, enemyHealth, enemyDmg, enemyAction);
+        }
     }
     
     // checking if we got the input settings right
