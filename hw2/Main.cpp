fed341f2aad740dc56d4702ba425d05a01af311b
fmgaring <fritzmgaring@gmail.com>
2026-09-05 23:25:43 +0800

    Finished HW2 + Added bouncing particles as bonus

--- hw2/Main.cpp
+++ hw2/Main.cpp
@@ -1,6 +1,7 @@
 #include <raylib.h>
 #include <raymath.h>
 #include <iostream>
+#include <fstream>
 
 struct Particle {
     bool isActive; //is active or nah
@@ -20,6 +21,15 @@ struct Particle {
         maxLifetime = life;
         color = col;
     }
+
+    void bounce() {
+        if (position.x <= 0) { direction.x = -direction.x; }
+        if (position.x >= 800) { direction.x = -direction.x; }
+        if (position.y <= 0) { direction.y = -direction.y; }
+        if (position.y >= 600) { direction.y = -direction.y; }
+    }
+
+
 };
 
 Color randomColor() {
@@ -38,10 +48,13 @@ float GetRandomFloat(float min, float max) {
 int main(){
 	InitWindow(800, 600, "Homework 02");
     int particleCount = 1000;
-    int rateX = 20;
-    int rateY = 20;
-    float timer = 1.0f;
+    float rateX = 50;
+    float rateY = 50;
+    // float timer = 1.0f;
+    float timePassed = 0.0f;
     int particleBirthed = 0;
+
+    int add_rateX, minus_rateX, add_rateY, minus_rateY, emit_particles_bottom, emit_particles_cursor;
 
     Particle* particles = new Particle[particleCount]; 
     for (int i = 0; i < particleCount; i++) {
@@ -49,75 +62,80 @@ int main(){
         particles[i].lifetime = 0;
     }
 
+    // configuration of keys
+    std::ifstream file("config.ini");
+    file >> add_rateX;
+    file.ignore(256, '\n');
+    file >> minus_rateX;
+    file.ignore(256, '\n');
+    file >> add_rateY;
+    file.ignore(256, '\n');
+    file >> minus_rateY;
+    file.ignore(256, '\n');
+    file >> emit_particles_bottom;
+    file.ignore(256, '\n');
+    file >> emit_particles_cursor;
+    file.ignore(256, '\n');
+    
+
 	while(!WindowShouldClose()){
 		float deltaTime = GetFrameTime();
 
-        
-
+    
         // Input Handling for Emission Rates etc. etc.
-        if (IsKeyPressed(KEY_LEFT)) {
+        if (IsKeyPressed(minus_rateX)) {
             rateX--;
             if (rateX <= 0) { rateX = 1; }
         }
-        else if (IsKeyPressed(KEY_RIGHT)) {
+        else if (IsKeyPressed(add_rateX)) {
             rateX++;
             if (rateX >= 50) { rateX = 50; }
         }
-        else if (IsKeyPressed(KEY_DOWN)) {
+        else if (IsKeyPressed(minus_rateY)) {
             rateY--;
-            if (rateY >= 0) { rateY = 1; }
+            if (rateY <= 0) { rateY = 1; }
         }
-        else if (IsKeyPressed(KEY_UP)) {
+        else if (IsKeyPressed(add_rateY)) {
             rateY++;
             if (rateY >= 50) { rateY = 50; }
         }
-
         
+        // interval between each particle spawn = 1 / rate
+        // then we use the interval as a Timer? kind of similar to sleep
 
-        // std::cout << "rate X: " << rateX << std::endl;
-        // std::cout << "rate Y: " << rateY << std::endl;
-        std::cout << deltaTime%1.0f << std::endl;
-        
-        if (timer > 0.0f) {
-            timer -= deltaTime;
-            if (timer <= 0.0f) {
-                particleBirthed = 0;
-                timer = 1.0f;
-            }
-        }
+        float intervalX = 1/rateX;
+        float intervalY = 1/rateY;    
         
         // Initialize particles based on #6
-        if (IsKeyDown(KEY_SPACE)) {
-            if (timer > 0.0f) {
-                timer -= deltaTime;
-                if (timer <= 0.0f) {
-                    particleBirthed = 0;
-                    timer = 1.0f;
+        if (IsKeyDown(emit_particles_bottom)) {
+            timePassed += deltaTime;
+            while (timePassed >= intervalX) {
+                for (int j = 0; j < particleCount; j++) {
+                    if (!particles[j].isActive) {
+                        particles[j].initialize(Vector2{400, 600}, Vector2{GetRandomFloat(-1.0f, 1.0f),(float)-1}, GetRandomValue(50, 100), GetRandomValue(2.0f, 5.0f), randomColor());
+                        particleBirthed++;
+                        // std::cout << particleBirthed << std::endl;
+                        break;
+                    }
                 }
-            }
-            for (int j = 0; j < particleCount; j++) {
-                if (!particles[j].isActive && particleBirthed < rateX) {
-                    particles[j].initialize(Vector2{400, 600}, Vector2{GetRandomFloat(-1.0f, 1.0f),(float)-1}, GetRandomValue(50, 100), GetRandomValue(2.0f, 5.0f), randomColor());
-                    particleBirthed++;
-                    std::cout << particleBirthed << std::endl;
-                    break;
-                }
-            }
-            
+                timePassed -= intervalX;
+            }         
         }
-        // std::cout << particleBirthed << std::endl;
-        
-        
 
         // Initialize particles based on #7
-        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-            for (int j = 0; j < rateY; j++) {
-                if (!particles[j].isActive) {
-                    particles[j].initialize(GetMousePosition(), Vector2{GetRandomFloat(-1.0f, 1.0f), GetRandomFloat(-1.0f, 1.0f)}, GetRandomValue(50, 100), GetRandomValue(2.0f, 5.0f), randomColor());
-                    particleBirthed++;
-                    break;
+        if (IsMouseButtonDown(emit_particles_cursor)) {
+            timePassed += deltaTime;
+            while (timePassed >= intervalY) {
+                for (int j = 0; j < rateY; j++) {
+                    if (!particles[j].isActive) {
+                        particles[j].initialize(GetMousePosition(), Vector2{GetRandomFloat(-1.0f, 1.0f), GetRandomFloat(-1.0f, 1.0f)}, GetRandomValue(50, 100), GetRandomValue(2.0f, 5.0f), randomColor());
+                        particleBirthed++;
+                        break;
+                    }
                 }
+                timePassed -= intervalY;
             }
+            
         }
         
         
@@ -130,6 +148,7 @@ int main(){
 
                 float lifeRatio = particles[i].lifetime / particles[i].maxLifetime;
                 particles[i].color.a = (lifeRatio * 255); // Fade
+                particles[i].bounce();
                 
                 //Clamp so it wouldn't go below 0
                 if (particles[i].color.a < 0) { 
@@ -152,7 +171,6 @@ int main(){
         }
 
 		EndDrawing();
-	
 	
 	}
 
