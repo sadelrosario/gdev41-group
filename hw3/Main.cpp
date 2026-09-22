6d7a7ac2434e61a7c134d07ae1bd9171b2de464b
Kuma <55661040+NxKuma@users.noreply.github.com>
2026-09-22 18:55:17 +0800

    Added Particles to the scene
    - Added Texture to it
    - Spawn function is separated for ease of use

--- hw3/Main.cpp
+++ hw3/Main.cpp
@@ -1,5 +1,6 @@
 #include <raylib.h>
 #include <raymath.h>
+#include <fstream>
 
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
@@ -51,6 +52,31 @@ struct Wall {
     }
 };
 
+
+struct Particle {
+    bool isActive; //is active or nah
+    Vector2 position; //particle position
+    Vector2 direction; // where the particle is going
+    float speed; //how fast is moving pixel per second
+    float rotation;
+    float lifetime; //how long particle is active in seconds
+    float maxLifetime; //how long particle is active in seconds
+    Color color; // particle color
+
+    void initialize(Vector2 pos, Vector2 dir, float spd, float life, Color col, float rot) {
+        isActive = true;
+        position = pos;
+        direction = dir;
+        speed = spd;
+        lifetime = life;
+        maxLifetime = life;
+        color = col;
+        rotation = rot;
+    }
+};
+int particleCount = 100;
+Particle* particles = new Particle[particleCount];
+
 void DrawBoard(Wall (&wallArray)[4], Ball (&holeArray)[4]){
     for(int i = 0; i < 4; i++){
         DrawRectangle(wallArray[i].position.x, wallArray[i].position.y, wallArray[i].width, wallArray[i].height, RED);
@@ -60,6 +86,26 @@ void DrawBoard(Wall (&wallArray)[4], Ball (&holeArray)[4]){
     };
 }
 
+float GetRandomFloat(float min, float max) {
+    float randomVal = min + (max - min) * ((float)GetRandomValue(0, 10000) / 10000.0f);
+    return randomVal;
+}
+
+// This is for spawning multiple at a time since the collision impulse will only happen once
+// This can be put when the pocket and ball collission is implemented 
+void SpawnParticles(Vector2 spawnPosition){
+    for (int j = 0; j < particleCount; j++) {
+        if (!particles[j].isActive) {
+            particles[j].initialize(spawnPosition, {GetRandomFloat(-1,1), -3.f}, GetRandomValue(25, 50), 1.0f, YELLOW, GetRandomFloat(1, 90) );
+            particles[j+1].initialize(spawnPosition, {GetRandomFloat(-1,1), -3.f},  GetRandomValue(25, 50), 1.0f, YELLOW, GetRandomFloat(1, 90) );
+            particles[j+2].initialize(spawnPosition, {GetRandomFloat(-1,1), -3.f},  GetRandomValue(25, 50), 1.0f, YELLOW, GetRandomFloat(1, 90) );
+            particles[j+3].initialize(spawnPosition, {GetRandomFloat(-1,1), -3.f},  GetRandomValue(25, 50), 1.0f, YELLOW, GetRandomFloat(1, 90) );
+            particles[j+4].initialize(spawnPosition, {GetRandomFloat(-1,1), -3.f},  GetRandomValue(25, 50), 1.0f, YELLOW, GetRandomFloat(1, 90) );
+            // std::cout << particleBirthed << std::endl;
+            break;
+        }
+    }
+};
 
 void CircleToCircleCollision(Ball& ball1, Ball&  ball2){
     Vector2 collision_normal = Vector2Subtract(ball1.position, ball2.position);
@@ -73,6 +119,9 @@ void CircleToCircleCollision(Ball& ball1, Ball&  ball2){
         float impulseNumerator = (1.0f + ELASTICITY_COEFFICIENT) * velAlongNormal;
         float impulseDenominator = Vector2DotProduct(collision_normal, collision_normal) * (ball1.inverse_mass + ball2.inverse_mass);
         float impulse = -(impulseNumerator / impulseDenominator);
+        // For Testing
+        // SpawnParticles(ball1.position);
+        
 
         ball1.velocity = ball1.velocity + Vector2Scale(collision_normal, impulse * ball1.inverse_mass);
         ball2.velocity = ball2.velocity - Vector2Scale(collision_normal, impulse * ball2.inverse_mass);
@@ -83,7 +132,7 @@ int main() {
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Homework 3 - Pool");
     float screenCenterX = WINDOW_WIDTH / 2;
     float screenCenterY = WINDOW_HEIGHT / 2;
-    
+    Texture texture = LoadTexture("five-pointed-star.png");
     //Initializing Balls
     float ballRadius = 25.0f;
     float ballMass = 1.0f;
@@ -169,16 +218,45 @@ int main() {
             accumulator -= TIMESTEP;
             
         }
+        
+        for (int i = 0; i < particleCount; i++) {
+            if (particles[i].isActive) {
+                particles[i].position.x += particles[i].direction.x * particles[i].speed * delta_time;
+                particles[i].position.y += particles[i].direction.y * particles[i].speed * delta_time;
+                particles[i].lifetime -= delta_time;
 
+                float lifeRatio = particles[i].lifetime / particles[i].maxLifetime;
+                particles[i].color.a = (lifeRatio * 255); // Fade
+                
+                //Clamp so it wouldn't go below 0
+                if (particles[i].color.a < 0) { 
+                    particles[i].color.a = 0;
+                }
+
+                if (particles[i].lifetime <= 0) {
+                    particles[i].isActive = false;
+                }
+            }
+        }
         BeginDrawing();
         ClearBackground(GREEN);
         DrawBoard(walls, pockets);
         for(int i = 0; i < ballCount; i++) {
             DrawCircleV(balls[i].position, balls[i].radius, balls[i].color);
         }
+        for (int i = 0; i < particleCount; i++) {
+            if (particles[i].isActive) {
+                // DrawCircle(particles[i].position.x, particles[i].position.y, 5, particles[i].color);
+                DrawTextureEx(texture, {particles[i].position.x, particles[i].position.y}, particles[i].rotation , 0.25, particles[i].color);
+            }
+        }
         // DrawCircleV(ball.position, ball.radius, ball.color);
         EndDrawing();
     }
+
+
+    UnloadTexture(texture);
+    delete[] particles;
     CloseWindow();
     return 0;
 }
